4582903290b2e7bde22618dfcf70a619f3883e08
andean <andean@thefthing.com>
2021-08-22 00:17:01 +0700

    test

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8000/api/'
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api/'
 
 Vue.config.productionTip = false
 
